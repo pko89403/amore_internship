a5a81d5883cc8fc7d15dddd71252e95ce8b10b7a
pko89403@gmail.com <pko89403@gmail.com>
2019-03-30 12:00:41 +0900

    window side

--- SimpleCNN_NE_GPU.py
+++ SimpleCNN_NE_GPU.py
@@ -1,22 +1,20 @@
 from __future__ import print_function
 
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-import keras
-from keras.utils import plot_model
-from keras.models import Model
+from tensorflow import keras
+from keras.callbacks import EarlyStopping
 from keras.layers import Input, Dense, Conv1D, MaxPool1D
 from keras.layers import Reshape, Flatten, Dropout, Concatenate, BatchNormalization
+from keras.models import Model
 from keras.preprocessing.text import *
+from keras.utils import plot_model
 from keras.utils import to_categorical
-from keras.callbacks import EarlyStopping
 from keras import backend as K
 import tensorflowjs as tfjs
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
-ODIR = 'modelCNN_NE'
-training_data = './Input_json/train.json.csv'
+training_data = '/content/drive/My Drive/colab_folder/Input_json/train.json.csv'
 df = pd.read_csv(training_data)
 
 # Create input and output Vector
@@ -32,30 +30,18 @@ Y = to_categorical(Y, num_classes=Y_CLASS)
 
 # Split into training and test data
 MAX_WORDS = 0
-MAX_LEN = 150
-
 
 def tokenizeData(x_datas):
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(x_datas)
     matrixes = tokenizer.texts_to_matrix(x_datas, mode='binary')
-    sequences = tokenizer.texts_to_sequences(x_datas)
-
-    maxlen = max([len(x) - 1 for x in sequences])
     vocab_size = len(tokenizer.word_index) + 1
-    print(maxlen, vocab_size)
-
-    global MAX_WORDS, MAX_LEN
+    global MAX_WORDS
     MAX_WORDS = vocab_size
-    MAX_LEN = 30
-
-    print(len(matrixes), len(matrixes[0]))
     return matrixes
-
 
 X = tokenizeData(X)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25)
-
 
 def CNN(max_words):
     inputs = Input(name='inputs', shape=(max_words,))
@@ -107,53 +93,28 @@ def CNN(max_words):
     dense3_bn = BatchNormalization()(dense3)
     dense3_drop = Dropout(0.5)(dense3_bn)
 
-
     global Y_CLASS
     output = Dense(units=Y_CLASS, activation='softmax')(dense3_drop)
     model = Model(inputs=inputs, outputs=output)
 
     return model
 
-
 model = CNN(MAX_WORDS)
-model.summary()
-plot_model(model, to_file='./' + ODIR + '/model.png')
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
 
 history = model.fit(X_train,
                     Y_train,
                     batch_size=200,
-                    epochs=1024,
+                    epochs=3,
                     validation_split=0.2,
                     callbacks=[EarlyStopping(monitor='val_loss',
                                              patience=5,
                                              )])
 
-# Plot training & validation accuracy values
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.savefig('./' + ODIR + '/train_acc.png')
-plt.clf()
-plt.cla()
-plt.close()
-# Plot training & validation loss values
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.savefig('./' + ODIR + '/train_loss.png')
-plt.clf()
-plt.cla()
-plt.close()
-
 score = model.evaluate(X_test, Y_test)
 print('Test loss: ', score[0])
 print('Test Accuracy: ', score[1])
 
-tfjs.converters.save_keras_model(model, ODIR)
+tfjs.converters.save_keras_model(model, '/content/drive/My Drive/colab_folder/')
