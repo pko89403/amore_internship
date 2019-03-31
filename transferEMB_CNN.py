22cf30639e82ba7ba19e17dea48e2fd18a9e870e
pko89403@gmail.com <pko89403@gmail.com>
2019-03-31 23:36:00 +0900

    smaller

--- transferEMB_CNN.py
+++ transferEMB_CNN.py
@@ -59,13 +59,13 @@ embedding= Embedding(   input_dim = vocabulary_size,
                         input_length = x_limitLen,
                         trainable=False)(inputs)
 
-conv_0 = Conv1D(filters=512, kernel_size=3, padding='valid', kernel_regularizer='l2', activation='relu')(embedding)
+conv_0 = Conv1D(filters=128, kernel_size=3, padding='valid', kernel_regularizer='l2', activation='relu')(embedding)
 conv_0_bn = BatchNormalization()(conv_0)
 
-conv_1 = Conv1D(filters=512, kernel_size=4, padding='valid',  kernel_regularizer='l2', activation='relu')(embedding)
+conv_1 = Conv1D(filters=128, kernel_size=4, padding='valid',  kernel_regularizer='l2', activation='relu')(embedding)
 conv_1_bn = BatchNormalization()(conv_1)
 
-conv_2 = Conv1D(filters=512, kernel_size=5, padding='valid',  kernel_regularizer='l2', activation='relu')(embedding)
+conv_2 = Conv1D(filters=128, kernel_size=5, padding='valid',  kernel_regularizer='l2', activation='relu')(embedding)
 conv_2_bn = BatchNormalization()(conv_2)
 
 maxpool_0 = MaxPool1D(pool_size=2, padding='valid')(conv_0_bn)
