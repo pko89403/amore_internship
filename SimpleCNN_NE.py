3e57f724e05f70f14ede587aa673dc0142776220
pko89403@gmail.com <pko89403@gmail.com>
2019-04-02 22:15:34 +0900

    filters >> 64

--- SimpleCNN_NE.py
+++ SimpleCNN_NE.py
@@ -55,15 +55,15 @@ def CNN(max_words):
 
     reshape = Reshape(target_shape=(max_words, 1,))(inputs)
 
-    conv_0 = Conv1D(filters=512, kernel_size=2, padding='valid', activation='relu')(reshape)
+    conv_0 = Conv1D(filters=64, kernel_size=2, padding='valid', activation='relu')(reshape)
     conv_0 = BatchNormalization()(conv_0)
     maxpool_0 = MaxPool1D(pool_size=2, padding='valid')(conv_0)
 
-    conv_1 = Conv1D(filters=512, kernel_size=3, padding='valid', activation='relu')(reshape)
+    conv_1 = Conv1D(filters=64, kernel_size=3, padding='valid', activation='relu')(reshape)
     conv_1 = BatchNormalization()(conv_1)
     maxpool_1 = MaxPool1D(pool_size=2, padding='valid')(conv_1)
 
-    conv_2 = Conv1D(filters=512, kernel_size=4, padding='valid', activation='relu')(reshape)
+    conv_2 = Conv1D(filters=64, kernel_size=4, padding='valid', activation='relu')(reshape)
     conv_2 = BatchNormalization()(conv_2)
     maxpool_2 = MaxPool1D(pool_size=2, padding='valid')(conv_2)
 
