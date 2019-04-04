eb5dfc4077e2bc0a3d10809cffe376f98e73476d
pko89403@gmail.com <pko89403@gmail.com>
2019-04-04 22:25:26 +0900

    CNN>>LSTM : HypOpt

--- CLSTM.py
+++ CLSTM.py
@@ -3,96 +3,76 @@ from __future__ import print_function
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-import io
-import json
-from keras.utils import plot_model
-from keras.models import Model
-from keras.layers import Input, Dense, Embedding, Conv1D, MaxPool1D, LSTM
-from keras.layers import Reshape, Flatten, Dropout, Concatenate
-from keras.optimizers import Adam
 from keras.preprocessing.text import *
 from keras.preprocessing import sequence
 from keras.utils import to_categorical
+
+from Data_proc import text2seq, MAX_LEN, MAX_WORD, Y_CLASS, TRAINING_PATH
+
+from keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input, Dense, Embedding, Conv1D, MaxPool1D, LSTM
+from keras.layers import Dropout, Concatenate
+from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
+
+from hyperopt import Trials, STATUS_OK, tpe
+from hyperas import optim
+from hyperas.distributions import choice, uniform
+
 import tensorflowjs as tfjs
 
-ODIR = 'CLSTM'
-training_data = './Input_json/train.json.csv'
+ODIR = 'CNN_LSTM_Model'
 
-df = pd.read_csv(training_data)
 
-# Create input and output Vector
-X = df.ingredients
-Y = df.cuisine
-Y_CLASS = 20 # NUM OF CLASS
+def CNN_LSTM(X_train, Y_train, X_test, Y_test):
+    filterCnt = {{choice([128,256,512,1024])}}
 
-# Process the labels
-le = LabelEncoder()
-Y = le.fit_transform(Y)
-Y = Y.reshape(-1,1)
-Y = to_categorical(Y, num_classes=Y_CLASS)
+    inputs = Input(name='inputs', shape=(MAX_LEN,))
+    embedding = Embedding(input_dim=MAX_WORD, output_dim=300, input_length=MAX_LEN)(inputs)
 
-# Split into training and test data
-MAX_WORDS = 0
-MAX_LEN = 0
+    conv_0 = Conv1D(filters=filterCnt, kernel_size={{choice([2,3,4,5,6])}}, padding='valid', kernel_initializer='normal', activation='relu')(embedding)
+    conv_1 = Conv1D(filters=filterCnt, kernel_size={{choice([2,3,4,5,6])}}, padding='valid', kernel_initializer='normal', activation='relu')(embedding)
+    conv_2 = Conv1D(filters=filterCnt, kernel_size={{choice([2,3,4,5,6])}}, padding='valid', kernel_initializer='normal', activation='relu')(embedding)
 
-def tokenizeData(x_datas):
-	tokenizer = Tokenizer()
-	tokenizer.fit_on_texts(x_datas)
-	sequences = tokenizer.texts_to_sequences(x_datas)
-    
-	maxlen = max([len(x) - 1 for x in sequences])
-	vocab_size = len(tokenizer.word_index) + 1
+    maxpool_0 = MaxPool1D(pool_size={{choice([2,3])}}, padding='valid')(conv_0)
+    maxpool_1 = MaxPool1D(pool_size={{choice([2,3])}}, padding='valid')(conv_1)
+    maxpool_2 = MaxPool1D(pool_size={{choice([2,3])}}, padding='valid')(conv_2)
 
-	global MAX_WORDS, MAX_LEN
-	MAX_WORDS = vocab_size
-	MAX_LEN = 30
-	sequences_matrix = sequence.pad_sequences(sequences, maxlen=MAX_LEN)
-	return sequences_matrix
+    concatenated = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
 
-X = tokenizeData(X)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25)
+    lstm_0 = LSTM({{choice([128,256,512,1024])}}, dropout={{uniform(0, 1)}}, recurrent_dropout={{uniform(0, 1)}})(concatenated)
+    dropout = Dropout({{uniform(0, 1)}})(lstm_0)
 
-def CLSTM(max_len, max_words):
-	inputs = Input(name='inputs', shape=(max_len,))
-	embedding = Embedding(input_dim=max_words, output_dim=64, input_length=max_len)(inputs)
+    output = Dense(units=Y_CLASS, activation='softmax')(dropout)
+    model = Model(inputs=inputs, outputs=output)
 
-	conv_0 = Conv1D(filters = 512, kernel_size=3, padding='valid', kernel_initializer='normal', activation='relu')(embedding)
-	conv_1 = Conv1D(filters = 512, kernel_size=4, padding='valid', kernel_initializer='normal', activation='relu')(embedding)
-	conv_2 = Conv1D(filters = 512, kernel_size=5, padding='valid', kernel_initializer='normal', activation='relu')(embedding)
-	  
-	maxpool_0 = MaxPool1D(pool_size=2, padding='valid')(conv_0)
-	maxpool_1 = MaxPool1D(pool_size=2, padding='valid')(conv_1)
-	maxpool_2 = MaxPool1D(pool_size=2, padding='valid')(conv_2)
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(lr={{choice([10 ** -3, 10 ** -2, 10 ** -1])}}),
+                  metrics=['accuracy'])
 
-	concatenated = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
-	dropout = Dropout(0.5)(concatenated)
+    model.fit(X_train,
+              Y_train,
+              batch_size={{choice([128, 256])}},
+              epochs=1024,
+              validation_split=0.2,
+              callbacks=[EarlyStopping(monitor='val_loss',patience=8,)],
+              verbose=2)
 
-	lstm_0 = LSTM(256, dropout=0.5, recurrent_dropout=0.5)(dropout)
-	
-	global Y_CLASS
-	output = Dense(units=Y_CLASS, activation='softmax')(lstm_0)
-	
-	model = Model(inputs=inputs, outputs=output)
-	
-	return model
+    score = model.evaluate(X_test, Y_test)
+    return {'loss': -score[1], 'status': STATUS_OK, 'model': model}
 
-model = CLSTM(MAX_LEN, MAX_WORDS)
+best_run, best_model = optim.minimize(model=CNN_LSTM,
+                                      data=text2seq,
+                                      algo=tpe.suggest,
+                                      max_evals=10,
+                                      trials=Trials(),
+                                      verbose=False)
 
-model.summary()
-plot_model(model, to_file='./'+ ODIR + '/model.png')
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-history = model.fit(	X_train,
-						Y_train,
-						batch_size=32,
-						epochs=256,
-						validation_split=0.25,
-						callbacks = [	EarlyStopping(	monitor='val_loss',
-										patience=10,
-										min_delta=0.0001)])
-
-score = model.evaluate(X_test, Y_test)
+best_model.summary()
+plot_model(best_model, to_file='./' + ODIR + '/model.png')
+X_train, X_test, Y_train, Y_test = text2seq()
+score = best_model.evaluate(X_test, Y_test)
 print('Test loss: ', score[0])
 print('Test Accuracy: ', score[1])
-
-tfjs.converters.save_keras_model(model, ODIR)
+print(best_run)
+tfjs.converters.save_keras_model(best_model, ODIR)
