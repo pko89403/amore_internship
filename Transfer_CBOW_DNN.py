7230c3222d756c5bd90942ad4e031925cb70fc47
pko89403@gmail.com <pko89403@gmail.com>
2019-04-05 09:21:19 +0900

    Continuous Bag Of Words Model HyperOpt

--- Transfer_CBOW_DNN.py
+++ Transfer_CBOW_DNN.py
@@ -1,123 +1,80 @@
 from __future__ import print_function
 
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-from keras.utils import to_categorical
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing import sequence
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from keras.preprocessing.text import *
+from keras.preprocessing import sequence
+from keras.utils import to_categorical
 
-ODIR = 'CBOW_DNN'
-TRAINING_DATA_PATH = './Input_json/train.json.csv'
-
-Y_CLASS = 20
-LOAD_WORD2VEC_WEIGHT = "./google300Weights.npy"
-
-df = pd.read_csv(TRAINING_DATA_PATH)
-
-X = df.ingredients
-Y = df.cuisine
-
-le = LabelEncoder()
-transform_Y = le.fit_transform(Y)
-transform_reshape_Y = transform_Y.reshape(-1, 1)
-transform_reshape_categorical_Y = to_categorical(transform_reshape_Y,
-                                                 num_classes=Y_CLASS)
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(X)
-sequences = tokenizer.texts_to_sequences(X)
-
-x_maxLen = max([len(x) - 1 for x in sequences])
-x_maxWords = len(tokenizer.word_index) + 1
-
-print('Tokenizing Result', x_maxLen, ', ', x_maxWords)
-x_limitLen = 30
-sequence_matrix = sequence.pad_sequences(sequences, maxlen= x_limitLen)
-
-X = sequence_matrix
-Y = transform_reshape_categorical_Y
-
-X_train, X_test, Y_train, Y_test = train_test_split(X,
-                                                    Y,
-                                                    test_size=0.25,
-                                                    )
-
+from Data_proc import text2seq, MAX_LEN, MAX_WORD, Y_CLASS, TRAINING_PATH
 import numpy as np
-embedding_weights = np.load(LOAD_WORD2VEC_WEIGHT)
-EMBEDDING_DIM = 300
 
-
-from keras.models import Model
-import keras.backend as K
-from keras.layers import Input, Embedding, Lambda, Dense, Dropout
 from keras.utils import plot_model
-
-inputs = Input(shape=(x_limitLen,))
-embedding = Embedding(input_dim = x_maxWords,
-                      output_dim= 300,
-                      weights = [embedding_weights],
-                      input_length=x_limitLen,
-                      trainable=False)(inputs)
-encoder = Lambda(lambda x : K.mean(x, axis=1),
-                 output_shape=lambda shape: (shape[0], ) + shape[2:])( embedding )
-
-dense0 = Dense(units=2048, activation = 'relu')(encoder)
-dropout0 = Dropout(0.5)(dense0)
-dense1 = Dense(units=2048, activation = 'relu')(dense0)
-dropout1 = Dropout(0.5)(dense1)
-dense2 = Dense(units=2048, activation = 'relu')(dense1)
-dropout2 = Dropout(0.5)(dense2)
-dense3 = Dense(units=2048, activation = 'relu')(dense2)
-
-
-output = Dense(units=Y_CLASS, activation = 'softmax')(dense3)
-
-model = Model(inputs = inputs, output=output)
-
-model.summary()
-plot_model(model, to_file='./' + ODIR + '/model.png')
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
+from keras.models import Model
+from keras.layers import Input, Dense, Embedding, Lambda, Dropout
+import keras.backend as K
+from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 
-history = model.fit(X_train,
-                    Y_train,
-                    batch_size=200,
-                    epochs=256,
-                    validation_split=0.2,
-                    callbacks = [EarlyStopping(monitor='val_loss',
-                                               patience=10,
-                                               min_delta=0.001)]
-                    )
-
-# Plot training & validation accuracy values
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.savefig('./' + ODIR + '/train_acc.png')
-plt.clf()
-plt.cla()
-plt.close()
-# Plot training & validation loss values
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.savefig('./' + ODIR + '/train_loss.png')
-plt.clf()
-plt.cla()
-plt.close()
-
-score = model.evaluate(X_test, Y_test)
-print('Test loss: ', score[0])
-print('Test Accuracy: ', score[1])
+from hyperopt import Trials, STATUS_OK, tpe
+from hyperas import optim
+from hyperas.distributions import choice, uniform
 
 import tensorflowjs as tfjs
-tfjs.converters.save_keras_model(model, ODIR)
+
+ODIR = 'CBOW_DNN'
+
+def CBOW_DNN(X_train, Y_train, X_test, Y_test):
+    EMBEDDING_W = np.load("./google300Weights.npy")
+
+    inputs=Input(shape=(MAX_LEN,))
+    embedding=Embedding(input_dim=MAX_WORD,
+                        output_dim= 300,
+                        weights = [EMBEDDING_W],
+                        input_length=MAX_LEN,
+                        trainable=False)(inputs)
+
+    encoder = Lambda(lambda x : K.mean(x, axis=1), output_shape=lambda shape: (shape[0], ) + shape[2:])(embedding)
+
+    dense0 = Dense(units={{choice([256,512,1024,2048])}}, activation = 'relu')(encoder)
+    dropout0 = Dropout({{uniform(0, 1)}})(dense0)
+    dense1 = Dense(units={{choice([256,512,1024,2048])}}, activation = 'relu')(dropout0)
+    dropout1 = Dropout({{uniform(0, 1)}})(dense1)
+    dense2 = Dense(units={{choice([256,512,1024,2048])}}, activation = 'relu')(dropout1)
+    dropout2 = Dropout({{uniform(0, 1)}})(dense2)
+    dense3 = Dense(units={{choice([256,512,1024,2048])}}, activation = 'relu')(dropout2)
+    dropout3 = Dropout({{uniform(0, 1)}})(dense3)
+
+    output = Dense(units=Y_CLASS, activation = 'softmax')(dropout3)
+    model = Model(inputs = inputs, output=output)
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adam(lr={{choice([10 ** -3, 10 ** -2, 10 ** -1])}}),
+                  metrics=['accuracy'])
+
+    model.fit(X_train, Y_train,
+              batch_size={{choice([128, 256])}},
+              epochs=1024,
+              validation_split=0.2,
+              callbacks=[EarlyStopping(monitor='val_loss', patience=8, )],
+              verbose=2)
+
+    score = model.evaluate(X_test, Y_test)
+    return {'loss': -score[1], 'status': STATUS_OK, 'model': model}
+
+best_run, best_model = optim.minimize(model=CBOW_DNN,
+                                      data=text2seq,
+                                      algo=tpe.suggest,
+                                      max_evals=10,
+                                      trials=Trials(),
+                                      verbose=False)
+
+best_model.summary()
+plot_model(best_model, to_file='./' + ODIR + '/model.png')
+X_train, X_test, Y_train, Y_test = text2seq()
+score = best_model.evaluate(X_test, Y_test)
+print('Test loss: ', score[0])
+print('Test Accuracy: ', score[1])
+print(best_run)
+tfjs.converters.save_keras_model(best_model, ODIR)
