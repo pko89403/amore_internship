9c8ceba106650fb6953b0dd1b48f9895b9ed6d74
pko89403@gmail.com <pko89403@gmail.com>
2019-04-06 12:28:03 +0900

    Transfer_CBOW_CNN

--- Transfer_CBOW_CNN.py
+++ Transfer_CBOW_CNN.py
@@ -8,11 +8,12 @@ from keras.preprocessing import sequence
 from keras.utils import to_categorical
 
 from Data_proc import text2seq, MAX_LEN, MAX_WORD, Y_CLASS, TRAINING_PATH
+import numpy as np
 
 from keras.utils import plot_model
 from keras.models import Model
-from keras.layers import Input, Dense, Embedding, Conv1D, MaxPooling1D, Dropout
-from keras.layers import Dropout, Concatenate
+from keras.layers import Input, Dense, Embedding, Lambda, Reshape, Conv1D, MaxPooling1D, Flatten, Concatenate, Dropout
+import keras.backend as K
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 
@@ -22,46 +23,58 @@ from hyperas.distributions import choice, uniform
 
 import tensorflowjs as tfjs
 
-ODIR = 'CNN_LSTM_Model'
 
+ODIR = 'Transfer_CBOW_CNN'
 
-def CNN_LSTM(X_train, Y_train, X_test, Y_test):
-    filterCnt = {{choice([128,256,512,1024])}}
+def CBOW_CNN(X_train, Y_train, X_test, Y_test):
+    EMBEDDING_W = np.load("./google300Weights.npy")
 
-    inputs = Input(name='inputs', shape=(MAX_LEN,))
-    embedding = Embedding(input_dim=MAX_WORD, output_dim=300, input_length=MAX_LEN)(inputs)
+    inputs = Input(shape=(MAX_LEN,))
+    embedding = Embedding(input_dim = MAX_WORD,
+                          output_dim= 300,
+                          weights = [EMBEDDING_W],
+                          input_length=MAX_LEN,
+                          trainable=False)(inputs)
 
-    conv_0 = Conv1D(filters=filterCnt, kernel_size={{choice([2,3,4,5,6])}}, padding='valid', kernel_initializer='normal', activation='relu')(embedding)
-    conv_1 = Conv1D(filters=filterCnt, kernel_size={{choice([2,3,4,5,6])}}, padding='valid', kernel_initializer='normal', activation='relu')(embedding)
-    conv_2 = Conv1D(filters=filterCnt, kernel_size={{choice([2,3,4,5,6])}}, padding='valid', kernel_initializer='normal', activation='relu')(embedding)
+    encoder=Lambda(lambda x : K.mean(x, axis=1),
+                    output_shape=lambda shape: (shape[0], 1) + shape[2:])( embedding )
+
+    reshape = Reshape((1, 300))(encoder)
+
+    conv_0 = Conv1D(filters={{choice([128,256,512,1024])}}, kernel_size={{choice([2,3,4])}}, padding='valid', activation='relu')(embedding)
+    conv_1 = Conv1D(filters={{choice([128,256,512,1024])}}, kernel_size={{choice([2,3,4])}}, padding='valid', activation='relu')(embedding)
+    conv_2 = Conv1D(filters={{choice([128,256,512,1024])}}, kernel_size={{choice([2,3,4])}}, padding='valid', activation='relu')(embedding)
 
     maxpool_0 = MaxPooling1D(pool_size={{choice([2,3])}}, padding='valid')(conv_0)
     maxpool_1 = MaxPooling1D(pool_size={{choice([2,3])}}, padding='valid')(conv_1)
     maxpool_2 = MaxPooling1D(pool_size={{choice([2,3])}}, padding='valid')(conv_2)
 
-    concatenated = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
+    flat_0 = Flatten()(maxpool_0)
+    flat_1 = Flatten()(maxpool_1)
+    flat_2 = Flatten()(maxpool_2)
 
-    lstm_0 = LSTM({{choice([128,256,512,1024])}}, dropout={{uniform(0, 1)}}, recurrent_dropout={{uniform(0, 1)}})(concatenated)
-    dropout = Dropout({{uniform(0, 1)}})(lstm_0)
+    concatenated = Concatenate(axis=1)([flat_0, flat_1, flat_2])
+    dropout = Dropout({{uniform(0, 1)}})(concatenated)
 
-    output = Dense(units=Y_CLASS, activation='softmax')(dropout)
-    model = Model(inputs=inputs, outputs=output)
+    output = Dense(units=Y_CLASS, activation = 'softmax')(dropout)
+
+    model = Model(inputs = inputs, output=output)
 
     model.compile(loss='categorical_crossentropy', optimizer=Adam(lr={{choice([10 ** -3, 10 ** -2, 10 ** -1])}}),
                   metrics=['accuracy'])
 
     model.fit(X_train,
               Y_train,
-              batch_size={{choice([128, 256])}},
+              batch_size={{choice([128, 256, 512])}},
               epochs=1024,
               validation_split=0.2,
-              callbacks=[EarlyStopping(monitor='val_loss',patience=8,)],
+              callbacks=[EarlyStopping(monitor='val_loss',patience=16,)],
               verbose=2)
 
     score = model.evaluate(X_test, Y_test)
     return {'loss': -score[1], 'status': STATUS_OK, 'model': model}
 
-best_run, best_model = optim.minimize(model=CNN_LSTM,
+best_run, best_model = optim.minimize(model=CBOW_CNN,
                                       data=text2seq,
                                       algo=tpe.suggest,
                                       max_evals=10,
